234b7f8c935e2868a486ae1f75560af2010cbf42
erjieisheree <erjieisheree@gmail.com>
2026-09-11 09:59:57 +0200

    <REFACTOR>(Fly-In): Changed TxtParser name to a more appropiate one. - Added a constants file which contains all strings used in simulator_converter
    Note: Probably simulator_converter should stay named as TxtParser, make it parse the txt into json or something else and some application use the json to get the DTO in order to start the program. Will take it in mind for the next project.

--- Milestone3/Fly-in/src/infrastructure/parsers/constants.py
+++ Milestone3/Fly-in/src/infrastructure/parsers/constants.py
@@ -1,9 +1,50 @@
-FILE_CONSTRAINTS_ERROR_MESSAGE = "Error at line {i}: The line must be empty, a"
-" comment (a line which starts with a '#') or a hub/connection. You can find t"
-"he constraints of them below:\n\nHub.\nhub: <name> <x_coordinate> <y_coordina"
-"te> [zone=<normal|blocked|restricted|priority> color=<string> max_drones=<pos"
-"itive_number>]\nFor starter and end hubs there is only one difference between"
-", they start with either start_hub or end_hub correspondingly.\n\nConnection."
-"\nconnection: <hub1>-<hub2> [max_link_capacity=<positive_number>]\nThe hubs m"
-"ust be created previously.\n\nTake in mind that the data between the clasps a"
-"re optional, but must follow the same syntaxis. If any is choosen."
+EMPTY_FILE_ERROR = "[ERROR]: This file is empty"
+FIRST_LINE_ERROR = (
+    "[ERROR] at line {i}: First line must follow this ""syntax 'nb_drones: <nu"
+    "mber>'"
+)
+NEGATIVE_NB_DRONES_ERROR = (
+    "[ERROR] at line {line_nb}: max_drones value must be positive.")
+FILE_CONSTRAINTS_ERROR = (
+    "Error at line {i}: The line must be empty, a comment (a line which starts"
+    " with a '#') or a hub/connection. You can find the constraints of them be"
+    "low:\n\nHub.\nhub: <name> <x_coordinate> <y_coordinate> [zone=<normal|blo"
+    "cked|restricted|priority> color=<string> max_drones=<positive_number>]\nF"
+    "or starter and end hubs there is only one difference between, they start "
+    "with either start_hub or end_hub correspondingly.\n\nConnection.\nconnect"
+    "ion: <hub1>-<hub2> [max_link_capacity=<positive_number>]\nThe hubs must b"
+    "e created previously.\n\nTake in mind that the data between the clasps ar"
+    "e optional, but must follow the same syntaxis. If any is choosen."
+)
+REPEATED_HUB_ID_ERROR = (
+    "[ERROR] at line {line_nb}: This hub name is already in use.")
+NEGATIVE_MAX_LINK_CAPACITY_ERROR = (
+    "[ERROR] at line {line_nb}: max_link_capacity value must be positive."
+)
+UNEXISTING_CONN_ERROR = (
+    "[ERROR] at line {line_nb}: A hub or both of the connection doesn't exist."
+)
+REPEATED_CONN_ERROR = (
+    "[ERROR] at line {line_nb}: The connection is repeated."
+)
+
+
+NB_DRONES_REGEX = r"^nb_drones:\s+\d+\s*\n$"
+SEARCH_NB_DRONES_REGEX = r"^nb_drones:\s+(?P<nb_drones>\d+)\s*\n?$"
+HUBS_REGEX = (
+    r"^(?:start_|end_)?hub:\s+[^\s\-]+\s+\-?\d+\s+\-?\d+(?:\s+\[(?:zone=(?:nor"
+    r"mal|blocked|restricted|priority)(?:\s+color=[a-zA-Z]+)?(?:\s+max_drones="
+    r"\d+)?|color=[a-zA-Z]+(?:\s+max_drones=\d+)?|max_drones=\d+)\])?\s*\n?$"
+)
+SEARCH_HUB_DATA_REGEX = (
+    r"^(start_|end_)?hub:\s+(?P<name>.*?)\s+(?P<x>.*?)\s+(?P<y>.*?)(?:\s+\[(?:"
+    r"zone=(?P<zone>(?:normal|blocked|restricted|priority))\s*)?(?:color=(?P<c"
+    r"olor>[a-zA-Z]+)\s*)?(?:max_drones=(?P<max_drones>\d+))?\])?\s*\n?$"
+)
+CONNECTION_REGEX = (
+    r"^connection:\s+[^\s\-]+\-[^\s\-]+(\s+\[max_link_capacity=\d+\])?\s*\n?$"
+)
+SEARCH_CONNECTION_DATA_REGEX = (
+    r"^.*?\s+(?P<hub1>.*?)\-(?P<hub2>.*?)(?:\s+\[max_link_capacity=(?P<max_lin"
+    r"k_capacity>\d+)\])?\s*\n?$"
+)
---
+++ Milestone3/Fly-in/src/infrastructure/parsers/simulator_converter.py
@@ -0,0 +1,153 @@
+import re
+
+
+from infrastructure.application.domain import Simulator, Hub, Zone
+from infrastructure.parsers.constants import (
+    # Regex rules
+    NB_DRONES_REGEX,
+    SEARCH_NB_DRONES_REGEX,
+    HUBS_REGEX,
+    SEARCH_HUB_DATA_REGEX,
+    CONNECTION_REGEX,
+    SEARCH_CONNECTION_DATA_REGEX,
+
+    # Errors
+    EMPTY_FILE_ERROR,
+
+    FIRST_LINE_ERROR,
+    NEGATIVE_NB_DRONES_ERROR,
+
+    FILE_CONSTRAINTS_ERROR,
+    REPEATED_HUB_ID_ERROR,
+    REPEATED_CONN_ERROR,
+    NEGATIVE_MAX_LINK_CAPACITY_ERROR,
+    UNEXISTING_CONN_ERROR
+    )
+
+
+class SimulatorConverter():
+    def get_simulator(self, file_dir: str) -> Simulator:
+        line: str = ""
+        nb_drones: int = 0
+        hubs: list[Hub] = []
+
+        i: int = 1
+
+        def get_nb_drones(line: str) -> int:
+            found = re.search(SEARCH_NB_DRONES_REGEX, line)
+            if found:
+                return int(found.group("nb_drones"))
+            else:
+                raise Exception("Superimpossible regex error")
+
+        def get_hub(line: str, line_nb: int) -> Hub:
+            name: str = ""
+            coordinates: tuple[int, int] = (0, 0)
+            zone: Zone = Zone.NORMAL
+            color: str = ""
+            max_drones: int = 1
+
+            hub_data = re.search(SEARCH_HUB_DATA_REGEX, line)
+            if hub_data:
+                name = hub_data.group("name")
+                coordinates = (
+                    int(hub_data.group("x")),
+                    int(hub_data.group("y"))
+                )
+                if hub_data.group("zone"):
+                    zone = Zone(hub_data.group("zone"))
+                if hub_data.group("color"):
+                    color = hub_data.group("color")
+                if hub_data.group("max_drones"):
+                    max_drones = int(hub_data.group("max_drones"))
+                    if max_drones == 0:
+                        raise ValueError(
+                            NEGATIVE_NB_DRONES_ERROR.format(line_nb=line_nb)
+                        )
+            if re.match(r"start_", line):
+                zone = Zone.START
+            elif re.match(r"end_", line):
+                zone = Zone.END
+            return Hub(name, coordinates, zone, color, max_drones)
+
+        def validate_hub(new_hub: Hub, line_nb: int) -> None:
+            for hub in hubs:
+                if new_hub.name == hub.name:
+                    raise ValueError(
+                        REPEATED_HUB_ID_ERROR.format(line_nb=line_nb))
+            return
+
+        def connect_hubs(
+            hub1: Hub,
+            hub2: Hub,
+            max_link_conn: int,
+            line_nb: int
+        ) -> None:
+            for hub in (hub1, hub2):
+                for hub_conns in hub.hubs:
+                    hub_conn, _ = hub_conns
+                    if (
+                        hub_conn.name == hub1.name or
+                        hub_conn.name == hub2.name
+                    ):
+                        raise ValueError(
+                            REPEATED_CONN_ERROR.format(line_nb=line_nb))
+            hub1.hubs += [(hub2, max_link_conn)]
+            hub2.hubs += [(hub1, max_link_conn)]
+            return
+
+        def create_connection(line: str, line_nb: int) -> None:
+            hub1: Hub | None = None
+            hub2: Hub | None = None
+            max_link_capacity: int = 1
+
+            # Get conn data
+            conn_data = re.search(SEARCH_CONNECTION_DATA_REGEX, line)
+            if conn_data:
+                for hub in hubs:
+                    if conn_data.group("hub1") == hub.name:
+                        hub1 = hub
+                    if conn_data.group("hub2") == hub.name:
+                        hub2 = hub
+                if conn_data.group("max_link_capacity"):
+                    max_link_capacity = int(
+                        conn_data.group("max_link_capacity"))
+                    if max_link_capacity == 0:
+                        raise ValueError(
+                            NEGATIVE_MAX_LINK_CAPACITY_ERROR
+                            .format(line_nb=line_nb)
+                        )
+            # Check both hub exists
+            if not hub1 or not hub2:
+                print([hub.name for hub in hubs])
+                raise ValueError(UNEXISTING_CONN_ERROR.format(line_nb=line_nb))
+            connect_hubs(hub1, hub2, max_link_capacity, line_nb)
+            return
+
+        with open(file_dir, "r") as f:
+            # Getting nb of drones
+            line = f.readline()
+            while not re.fullmatch(NB_DRONES_REGEX, line):
+                if line == "\n" or line.startswith('#'):
+                    line = f.readline()
+                elif not line:
+                    raise ValueError(EMPTY_FILE_ERROR)
+                else:
+                    raise ValueError(FIRST_LINE_ERROR.format(i=i))
+                i += 1
+            nb_drones = get_nb_drones(line)
+            for i, line in enumerate(f, i+1):
+                # Getting hubs
+                if line == "\n" or line.startswith('#'):
+                    pass
+                elif re.fullmatch(HUBS_REGEX, line):
+                    hub = get_hub(line, i)
+                    validate_hub(hub, i)
+                    hubs += [hub]
+                # Getting connections
+                elif re.fullmatch(CONNECTION_REGEX, line):
+                    create_connection(line, i)
+                else:
+                    raise ValueError(
+                        FILE_CONSTRAINTS_ERROR.format(i=i))
+        return Simulator(nb_drones, hubs)
